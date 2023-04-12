e34ea6a4e9e715164559828b5b11f1284acd541e
Nicolas Lopez Pineda <nicolas.pineda@correounivalle.edu.co>
2023-04-12 17:09:58 -0500

    primer commit para desplegar

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -9,21 +9,46 @@ import {
   Typography,
   Hidden,
   CardMedia,
+  Drawer,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  AppBar,
+  Toolbar,
+  Slide
 } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import ReactWhatsapp from "react-whatsapp";
 import { motion } from "framer-motion";
 import Card from "@mui/joy/Card";
 import { Grid, Paper } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { courseContent } from "./constCourses";
+import CourseDetails from "./CourseDetails";
 
 const Courses = () => {
-  
+  const [course, setCourse] = useState();
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const openDrawer = (item) => {
+    setOpen(true);
+    setCourse(item);
+    setMessage(`Hola. Me gustaría inscribirme al curso ${item.title}`);
+    console.log('drawer openned');
+  };
+  const handleClose = () =>{
+    setOpen(false);
+  }
   return (
     <Container sx={{ backgroundColor: "", height: "auto" }}>
       <Typography
         variant="h3"
-        sx={{ color: "#efb810", fontFamily: "Merriweather", textAlign:"center" }}
+        sx={{
+          color: "#efb810",
+          fontFamily: "Merriweather",
+          textAlign: "center",
+        }}
       >
         Cursos
       </Typography>
@@ -32,20 +57,27 @@ const Courses = () => {
         animate={{ opacity: 1, transition: { duration: 1 } }}
         exit={{ opacity: 0 }}
       >
-        <Grid container spacing={2} sx={{ my: 2 }}>
-          {courseContent.map((item,index) => (
-            <Grid item xs={12} sm={6} md={4} lg={4}>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            my: 2,
+            justifyContent: { xs: "center", md: "center", lg: "normal" },
+          }}
+        >
+          {courseContent.map((item, index) => (
+            <Grid item xs={10} sm={8} md={6} lg={4}>
               <Paper
                 elevation={3}
                 sx={{
-                  height: 330,
+                  height: { sm: "auto", md: 330 },
                   overflowX: "hidden",
                   display: "flex",
                   flexWrap: "wrap",
                   alignContent: "space-between",
                 }}
                 component={motion.div}
-                whileHover={{scale:1.05}}
+                whileHover={{ scale: 1.05 }}
               >
                 <CardMedia
                   component="img"
@@ -55,32 +87,126 @@ const Courses = () => {
                   loading="lazy"
                   alt="asf"
                 ></CardMedia>
-                <CardContent sx={{width:"100%", textAlign:"center"}}>
-                  <Typography variant="h6" sx={{ fontWeight: "600", fontFamily: "Merriweather" }}>
+                <CardContent sx={{ width: "100%", textAlign: "center" }}>
+                  <Typography
+                    variant="h6"
+                    sx={{ fontWeight: "600", fontFamily: "Merriweather" }}
+                  >
                     {item.title}
                   </Typography>
-                  <Typography variant="body1" sx={{fontFamily: "Merriweather", my:1}}>
+                  <Typography
+                    variant="body1"
+                    sx={{ fontFamily: "Merriweather", my: 1 }}
+                  >
                     {item.subtitle1}
                   </Typography>
-                  <Typography variant="body2" sx={{fontFamily: "Merriweather"}}>
+                  <Typography
+                    variant="body2"
+                    sx={{ fontFamily: "Merriweather" }}
+                  >
                     {item.subtitle2}
                   </Typography>
                 </CardContent>
                 <Box sx={{ display: "flex", justifyContent: "center" }}></Box>
-                <Button
-                  key={`title+${index}`}
-                  size="sm"
-                  aria-label="Explore Bahamas Islands"
-                  sx={{ ml: "auto", fontWeight: 800, width: "100%", color:"black", "&:hover":{color:"#efb810"} }}
-                  to={"/coursesDetails"}
-                  state={{course:item}}
-                  component={Link}
-                >
-                  Ver más
-                </Button>
+                <Hidden only={["xs", "sm"]}>
+                  <Button
+                    key={`title+${index}`}
+                    size="sm"
+                    aria-label="Explore Bahamas Islands"
+                    sx={{
+                      ml: "auto",
+                      fontWeight: 800,
+                      width: "100%",
+                      color: "black",
+                      "&:hover": { color: "#efb810" },
+                    }}
+                    onClick={() => openDrawer(item)}
+                  >
+                    Ver más
+                  </Button>
+                </Hidden>
+                <Hidden only={["md", "lg"]}>
+                  <Button
+                    key={`title+${index}`}
+                    size="sm"
+                    aria-label="Explore Bahamas Islands"
+                    sx={{
+                      ml: "auto",
+                      fontWeight: 800,
+                      width: "100%",
+                      color: "black",
+                      "&:hover": { color: "#efb810" },
+                    }}
+                    onClick={() => openDrawer(item)}
+                  >
+                    Ver más
+                  </Button>
+                </Hidden>
               </Paper>
             </Grid>
           ))}
+          <Hidden>
+          <Dialog
+          open={open}
+          scroll="paper"
+          onClose={() => handleClose()}
+          sx={{height:{xs:"auto",md:650}, my:{xs:0 ,md:4}, mx:{xs:0 ,md:4}, mt:{xs:5,md:4}}}
+          fullScreen
+        >
+          <IconButton
+                edge="start"
+                onClick={handleClose}
+                aria-label="close"
+                sx={{position:"fixed", color:"#fff", ml:1,mt:1}}
+              >
+                <CloseIcon />
+              </IconButton>
+          <DialogContent
+            sx={{
+              backgroundColor: "#04060b",
+              height: "450px",
+            }}
+          >
+            <CourseDetails courseDetail={course}></CourseDetails>
+          </DialogContent>
+          <DialogActions
+            sx={{
+              position: "sticky",
+              bottom: 0,
+              backgroundColor: "black",
+              display: "flex",
+              justifyContent: "center",
+              py: 1,
+              width: "100%",
+            }}
+            component={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                width: { xs: "100%", md: "30%" },
+                backgroundColor: "#efb603",
+                fontSize: "18px",
+                borderColor: "#000",
+                color: "#000",
+                fontWeight: "bold",
+                "&:hover": {
+                  backgroundColor: "#ffffff",
+                  color: "#000000",
+                },
+              }}
+              component={ReactWhatsapp}
+              number="57-322-617-4846"
+              message={message}
+            >
+              Inscríbete Ahora
+            </Button>
+          </DialogActions>
+        </Dialog>
+          </Hidden>
         </Grid>
       </motion.div>
     </Container>
--- src/components/Courses/CoursesList.jsx
+++ src/components/Courses/CoursesList.jsx
@@ -5,9 +5,7 @@ import {
   ListItem,
   ListItemButton,
   Paper,
-} from "@mui/material";
-import { motion } from "framer-motion";
-import {
+  Drawer,
   CardActionArea,
   CardActions,
   CardContent,
@@ -21,9 +19,10 @@ import {
   Hidden,
   Card,
 } from "@mui/material";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import CourseDetails from "./CourseDetails";
-import ReactWhatsapp from 'react-whatsapp';
+import ReactWhatsapp from "react-whatsapp";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { courseContent } from "./constCourses";
@@ -31,164 +30,254 @@ import { courseContent } from "./constCourses";
 const CoursesDetails = () => {
   const [course, setCourse] = useState();
   const [stateDetails, setStateDetails] = useState(true);
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
+  const [open, setOpen] = useState(false);
   let location = useLocation();
   const sendCourse = (item) => {
     setStateDetails(false);
     setTimeout(() => {
       setStateDetails(true);
       setCourse(item);
-      setMessage(`Hola. Me gustaría inscribirme al curso ${item.title}`)
+      setMessage(`Hola. Me gustaría inscribirme al curso ${item.title}`);
     }, 100);
   };
   useEffect(() => {
-    console.log('--inicio courses list--')
-    console.log(location.state.course)
-    console.log(course)
-    console.log('--fin courses list--')
-    const c = location.state.course !== null? location.state.course : courseContent[0]; 
-      // setCourse(location.state.course)
-      setCourse(c);
-      setMessage(`Hola. Me gustaría inscribirme al curso ${c.title}`)
-      console.log("hiiii"+c.title)
+    console.log("--inicio courses list--");
+    console.log(location.state.course);
+    console.log(course);
+    console.log("--fin courses list--");
+    const c =
+      location.state.course !== null ? location.state.course : courseContent[0];
+    // setCourse(location.state.course)
+    setCourse(c);
+    setMessage(`Hola. Me gustaría inscribirme al curso ${c.title}`);
+    console.log("hiiii" + c.title);
   }, []);
 
   return (
-    <Box
-      sx={{
-        width: "90%",
-        overflowX: "hidden",
-        overflowY: "hidden",
-        overflow: "hidden",
-        scrollbarWidth: "none",
-        mx: "auto",
-        backgroundColor: "#04060b",
-        mb: 2,
-      }}
-      component={motion.div}
-      initial={{ x: -2000 }}
-      animate={{ x: 0, transition: { duration: 1 } }}
-      exit={{ opacity: 0 }}
-    >
-      <Grid container sx={{ width: "100%" }}>
-        <Grid
-          item
-          sm={6}
-          md={4}
-          lg={4}
+    <>
+      <Hidden only={["xs", "sm"]}>
+        <Box
           sx={{
-            backgroundColor: "#04060b",
-            height: "570px",
+            width: "90%",
             overflowX: "hidden",
-          }}
-        >
-          <List>
-            {courseContent.map((item, index) => (
-              <ListItem
-                key={index}
-                onClick={() => {
-                  sendCourse(item);
-                }}
-              >
-                <Paper
-                  sx={{ width: "80%", mb: 1, cursor: "pointer" }}
-                  component={motion.div}
-                  initial={{ height: "50%", width: "100%" }}
-                  whileHover={{ scale: 1.02 }}
-                >
-                  <AspectRatio
-                    minHeight="70px"
-                    maxHeight="150px"
-                    sx={{ mb: 1 }}
-                    component={CardMedia}
-                  >
-                    <img
-                      src="images/logo-lina-quesada.png"
-                      srcSet="images/logo-lina-quesada.png 2x"
-                      loading="lazy"
-                      alt="asf"
-                    />
-                  </AspectRatio>
-                  <CardContent>
-                    <Typography
-                      variant="h4"
-                      component="div"
-                      sx={{ fontSize: { sm: "16px", md: "20px" } }}
-                    >
-                      {item.title}
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                      {item.subtitle1}
-                    </Typography>
-                    <Typography variant="subtitle2" component="div">
-                      {item.subtitle2}
-                    </Typography>
-                  </CardContent>
-                </Paper>
-              </ListItem>
-            ))}
-          </List>
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          sm={6}
-          md={8}
-          lg={8}
-          sx={{
+            overflowY: "hidden",
+            overflow: "hidden",
+            scrollbarWidth: "none",
+            mx: "auto",
             backgroundColor: "#04060b",
-            height: "570px",
-            overflowX: "hidden",
+            mb: 2,
           }}
+          component={motion.div}
+          initial={{ x: -2000 }}
+          animate={{ x: 0, transition: { duration: 1 } }}
+          exit={{ opacity: 0 }}
         >
-          <Paper elevation={3} square sx={{ backgroundColor: "#04060b" }}>
-            <Box
+          <Grid container sx={{ width: "100%" }}>
+            <Grid
+              item
+              sm={6}
+              md={4}
+              lg={4}
               sx={{
                 backgroundColor: "#04060b",
-                height: "",
+                height: "570px",
+                overflowX: "hidden",
               }}
             >
-              {stateDetails && <CourseDetails courseDetail={course}></CourseDetails>}
-            </Box>
-            {stateDetails && <Box
+              <List>
+                {courseContent.map((item, index) => (
+                  <ListItem
+                    key={index}
+                    onClick={() => {
+                      sendCourse(item);
+                      //setOpen(true);
+                    }}
+                  >
+                    <Paper
+                      sx={{ width: "80%", mb: 1, cursor: "pointer" }}
+                      component={motion.div}
+                      initial={{ height: "50%", width: "100%" }}
+                      whileHover={{ scale: 1.02 }}
+                    >
+                      <AspectRatio
+                        minHeight="70px"
+                        maxHeight="150px"
+                        sx={{ mb: 1 }}
+                        component={CardMedia}
+                      >
+                        <img
+                          src="images/logo-lina-quesada.png"
+                          srcSet="images/logo-lina-quesada.png 2x"
+                          loading="lazy"
+                          alt="asf"
+                        />
+                      </AspectRatio>
+                      <CardContent>
+                        <Typography
+                          variant="h4"
+                          component="div"
+                          sx={{ fontSize: { sm: "16px", md: "20px" } }}
+                        >
+                          {item.title}
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                          {item.subtitle1}
+                        </Typography>
+                        <Typography variant="subtitle2" component="div">
+                          {item.subtitle2}
+                        </Typography>
+                      </CardContent>
+                    </Paper>
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              sm={6}
+              md={8}
+              lg={8}
               sx={{
-                position: "sticky",
-                bottom: 0,
-                backgroundColor: "black",
-                display: "flex",
-                justifyContent: "center",
-                py:1
+                backgroundColor: "#04060b",
+                height: "570px",
+                overflowX: "hidden",
               }}
-              component={motion.div}
-              initial={{opacity:0}}
-              animate={{opacity:1}}
-              transition={{duration:1}}
             >
-              <Button
-                variant="contained"
-                sx={{
-                  width:{xs:"100%", md:"30%"},
-                  backgroundColor: "#efb603",
-                  fontSize: "18px",
-                  borderColor: "#000",
-                  color: "#000",
-                  fontWeight:"bold",
-                  "&:hover": {
-                    backgroundColor: "#ffffff",
-                    color: "#000000",
-                  },
-                }}
-                component={ReactWhatsapp} 
-                number="57-322-617-4846" 
-                message={message}
-              >
-                Inscríbete Ahora
-              </Button>
-            </Box>}
-          </Paper>
-        </Grid>
-      </Grid>
-    </Box>
+              <Paper elevation={3} square sx={{ backgroundColor: "#04060b" }}>
+                <Box
+                  sx={{
+                    backgroundColor: "#04060b",
+                    height: "",
+                  }}
+                >
+                  {stateDetails && (
+                    <CourseDetails courseDetail={course}></CourseDetails>
+                  )}
+                </Box>
+                {stateDetails && (
+                  <Box
+                    sx={{
+                      position: "sticky",
+                      bottom: 0,
+                      backgroundColor: "black",
+                      display: "flex",
+                      justifyContent: "center",
+                      py: 1,
+                    }}
+                    component={motion.div}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1 }}
+                  >
+                    <Button
+                      variant="contained"
+                      sx={{
+                        width: { xs: "100%", md: "30%" },
+                        backgroundColor: "#efb603",
+                        fontSize: "18px",
+                        borderColor: "#000",
+                        color: "#000",
+                        fontWeight: "bold",
+                        "&:hover": {
+                          backgroundColor: "#ffffff",
+                          color: "#000000",
+                        },
+                      }}
+                      component={ReactWhatsapp}
+                      number="57-322-617-4846"
+                      message={message}
+                    >
+                      Inscríbete Ahora
+                    </Button>
+                  </Box>
+                )}
+              </Paper>
+            </Grid>
+          </Grid>
+        </Box>
+      </Hidden>
+      <Hidden only={["md", "lg"]}>
+        <Box
+          sx={{
+            width: {xs:"90%", sm:"80%"},
+            overflowX: "hidden",
+            overflowY: "hidden",
+            overflow: "hidden",
+            scrollbarWidth: "none",
+            mx: "auto",
+            backgroundColor: "#04060b",
+            mb: 2,
+          }}
+          component={motion.div}
+          initial={{ x: -2000 }}
+          animate={{ x: 0, transition: { duration: 1 } }}
+          exit={{ opacity: 0 }}
+        >
+          <Grid container sx={{ width: {xs:"100%", sm:"80%"} }}>
+            <Grid
+              item
+              sm={12}
+              md={12}
+              lg={12}
+              sx={{
+                backgroundColor: "#04060b",
+                overflowX: "hidden",
+              }}
+            >
+              <List>
+                {courseContent.map((item, index) => (
+                  <ListItem
+                    key={index}
+                    onClick={() => {
+                      //sendCourse(item);
+                      setOpen(true);
+                    }}
+                  >
+                    <Paper
+                      sx={{ width: "80%", mb: 1, cursor: "pointer" }}
+                      component={motion.div}
+                      initial={{ height: "50%", width: "100%" }}
+                      whileHover={{ scale: 1.02 }}
+                    >
+                      <AspectRatio
+                        minHeight="70px"
+                        maxHeight="150px"
+                        sx={{ mb: 1 }}
+                        component={CardMedia}
+                      >
+                        <img
+                          src="images/logo-lina-quesada.png"
+                          srcSet="images/logo-lina-quesada.png 2x"
+                          loading="lazy"
+                          alt="asf"
+                        />
+                      </AspectRatio>
+                      <CardContent>
+                        <Typography
+                          variant="h4"
+                          component="div"
+                          sx={{ fontSize: { sm: "16px", md: "20px" } }}
+                        >
+                          {item.title}
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                          {item.subtitle1}
+                        </Typography>
+                        <Typography variant="subtitle2" component="div">
+                          {item.subtitle2}
+                        </Typography>
+                      </CardContent>
+                    </Paper>
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+          </Grid>
+        </Box>
+      </Hidden>
+    </>
   );
 };
 
