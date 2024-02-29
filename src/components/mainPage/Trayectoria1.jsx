ea0b9d9119b391f44e4706544343c5674b582e21
nicolaslopi <nicolaslopi00@gmail.com>
2024-02-29 09:47:30 -0500

    commit}

--- src/components/mainPage/Trayectoria1.jsx
+++ src/components/mainPage/Trayectoria1.jsx
@@ -9,10 +9,10 @@ const Trayectoria1 = () => {
     return (
         <Container sx={{ bgcolor: "", pt:2}}>
             <Grid container sx={{display:"flex", justifyContent:"space-around"}}>
-                <Grid item md={6} sx={{display:"flex", justifyContent:"center", px: {xs:5, md:0}}}>
-                    <img src='images/Trayectoria 2.png' style={{width:'auto', height:"100vh", aspectRatio:'1:1'}}></img>
+                <Grid item md={6} sx={{display:"flex", justifyContent:"center"}}>
+                    <img src='images/Trayectoria 2.png' style={{width:'auto', height:"100vh", marginLeft:{xs:3, md:0}}}></img>
                 </Grid>
-                <Grid item md={6} sx={{display:"flex", alignItems:"center", textAlign:{xs:"center", md:""}, px:4, pt:{xs:6, md:3}, bgcolor:"", fontFamily:"Quicksand"}}>
+                <Grid item md={6} sx={{display:"flex", alignItems:"center", textAlign:{xs:"center", md:""}, px:4, pt:{xs:6, md:3}, bgcolor:"", fontFamily:"Quicksand", mb:{xs:2, md:0}}}>
                     <p style={{fontSize:"23px"}}>Mis logros incluyen múltiples victorias en campeonatos internacionales y nacionales de Microblading, entre ellos el 1er Lugar en el Campeonato Internacional de México. Soy Phibrows artista Royal Microblading y Subcampeona en Microblading en Colombia.
                     <br></br><br></br>Hoy, continúo construyendo mi legado a través de la Academia Lina Quesada PMU, formando a nuevos talentos y consolidando una impresionante trayectoria en el mundo de la micropigmentación.
                     </p>
