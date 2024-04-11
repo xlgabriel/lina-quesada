9f17a22c9d2ef09d295ee54f72a444646ca02e29
unknown <nicolaslopi>
2024-04-10 20:48:34 -0500

    fotos cambiadas

--- src/components/mainPage/Trayectoria1.jsx
+++ src/components/mainPage/Trayectoria1.jsx
@@ -9,11 +9,11 @@ const Trayectoria1 = () => {
     return (
         <Container sx={{ bgcolor: "", pt:2}}>
             <Grid container sx={{display:"flex", justifyContent:"space-around"}}>
-                <Grid item md={12} sx={{display:{xs:"block", md:"none"}, justifyContent:"center", mr:2}}>
-                    <img src='images/trayectoria 3.png' style={{width:'auto', height:"85vh"}}></img>
+                <Grid item md={12} sx={{display:{xs:"flex", md:"none"}, justifyContent:"center"}}>
+                    <img src='images/trayectoria 3.png' style={{width:'100%', height:"auto"}}></img>
                 </Grid>
                 <Grid item md={6} xs={12} sx={{display:{xs:"none", md:"flex"}, justifyContent:"center"}}>
-                    <img src='images/trayectoria 3.png' style={{width:'auto', height:"90vh"}}></img>
+                    <img src='images/trayectoria 3.png' style={{width:'100%', height:""}}></img>
                 </Grid>
                 <Grid item md={6} sx={{display:"flex", alignItems:"center", textAlign:{xs:"center", md:""}, px:3, pt:{xs:6, md:3}, bgcolor:"", fontFamily:"Quicksand", mb:{xs:2, md:0}}}>
                     <p style={{fontSize:"23px"}}>Mis logros incluyen múltiples victorias en campeonatos internacionales y nacionales de Microblading, entre ellos el 1er Lugar en el Campeonato Internacional de México. Soy Phibrows artista Royal Microblading y Subcampeona en Microblading en Colombia.
