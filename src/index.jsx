4ef32c135ccf5eb1e61de044c67e8f02b78408ee
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-05-24 23:04:42 -0400

    refactor: cleaning code
    Please, provide the current state of the website so I can make changes with the actual project. Lina wants me to help her with some minor changes.

--- src/index.jsx
+++ src/index.jsx
@@ -16,33 +16,28 @@ import Navbar from "./components/Navbar/NavbarDrawer";
 import Footer from "./components/mainPage/Footer";
 // import reportWebVitals from './reportWebVitals';
 import { AnimatePresence } from "framer-motion";
-import { Box,Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import MainPage from "./components/mainPage/MainPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <AnimatePresence>
-      <Navbar></Navbar>
-      <Grid>
-        <Grid item xs={12} md={12}>
-        <Box sx={{ pt: 12, backgroundColor:'' }}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/coursesDetails" element={<CoursesList />} />
-        </Routes>
-      </Box>
-        </Grid>
-        <Grid item xs={12} md={12}>
-        <Footer/>
-        </Grid>
-      </Grid>
-    </AnimatePresence>
-  </BrowserRouter>
+    <BrowserRouter>
+        <AnimatePresence>
+            <Navbar></Navbar>
+            <Grid>
+                <Grid item xs={12} md={12}>
+                    <Box sx={{ pt: 12, backgroundColor: "" }}>
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/courses" element={<Courses />} />
+                            <Route path="/coursesDetails" element={<CoursesList />} />
+                        </Routes>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={12}>
+                    <Footer />
+                </Grid>
+            </Grid>
+        </AnimatePresence>
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
